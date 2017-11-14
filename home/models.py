e1ea0e17c4370653ca296030e4eff65c09ab0ed6
Alonzo Smith <smitha80lasalleclass@gmail.com>
2017-11-13 20:14:28 -0500

    Added billing and payment information to profile model

--- home/models.py
+++ home/models.py
@@ -5,6 +5,7 @@ from django.dispatch import receiver
 
 # Create your models here.
 
+
 class Books(models.Model):
     book_name = models.CharField(default='', blank=False, max_length=50)
     book_author = models.CharField(default='anonymous', blank=False, max_length=50)
@@ -15,21 +16,108 @@ class Books(models.Model):
     book_hardback = models.BooleanField(default=False, blank=False)
     book_retailPrice = models.DecimalField(default=0, blank=False, max_digits=5, decimal_places=2)
 
+
 # Need to create relationships for user and book_name
 class Review(models.Model):
     user = models.CharField(max_length=30)
     review_date = models.DateField(blank=False)
     book_name = models.CharField(max_length=50, blank=False)
-    review_rating = models.DecimalField(max_digits=2,decimal_places=1, blank=False)
+    review_rating = models.DecimalField(max_digits=2, decimal_places=1, blank=False)
     review_name = models.CharField(max_length=30, blank=False)
     review_content = models.CharField(max_length=500, blank=True)
 
+
+# TODO: Add validation to model fields in back or front end. Not necessary for prototype
+# TODO: Possibly add shipping related information instead of just billing. This includes implementing "Same as shipping"
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     steam_id = models.CharField(default='', blank=True, max_length=20)
-    # bio = models.TextField(max_length=500, blank=True)
-    # location = models.CharField(max_length=30, blank=True)
-    # birth_date = models.DateField(null=True, blank=True)
+
+    # Billing Fields.
+    address = models.CharField(default='', blank=True, max_length=70)
+    phone_num = models.IntegerField(max_length=10, blank=True)
+    city = models.CharField(default='', blank=True, max_length=50)
+    zip_code = models.IntegerField(max_length=5, blank=True)
+
+    STATE_CHOICES = (
+        ('AK', 'Alabama'),
+        ('AL', 'Alaska'),
+        ('AZ', 'Arizona'),
+        ('AR', 'Arkansas'),
+        ('CA', 'California'),
+        ('CO', 'Colorado'),
+        ('CT', 'Connecticut'),
+        ('DE', 'Delaware'),
+        ('FL', 'Florida'),
+        ('GA', 'Georgia'),
+        ('HI', 'Hawaii'),
+        ('ID', 'Idaho'),
+        ('IL', 'Illinois'),
+        ('IN', 'Indiana'),
+        ('IA', 'Iowa'),
+        ('KS', 'Kansas'),
+        ('KY', 'Kentucky'),
+        ('LA', 'Louisiana'),
+        ('ME', 'Maine'),
+        ('MD', 'Maryland'),
+        ('MA', 'Massachusetts'),
+        ('MI', 'Michigan'),
+        ('MN', 'Minnesota'),
+        ('MS', 'Mississippi'),
+        ('MO', 'Missouri'),
+        ('MT', 'Montana'),
+        ('NE', 'Nebraska'),
+        ('NV', 'Nevada'),
+        ('NH', 'New Hampshire'),
+        ('NJ', 'New Jersey'),
+        ('NM', 'New Mexico'),
+        ('NY', 'New York'),
+        ('NC', 'North Carolina'),
+        ('ND', 'North Dakota'),
+        ('OH', 'Ohio'),
+        ('OK', 'Oklahoma'),
+        ('OR', 'Oregon'),
+        ('PA', 'Pennsylvania'),
+        ('RI', 'Rhode Island'),
+        ('SC', 'South Carolina'),
+        ('SD', 'South Dakota'),
+        ('TN', 'Tennessee'),
+        ('TX', 'Texas'),
+        ('UT', 'Utah'),
+        ('VT', 'Vermont'),
+        ('VA', 'Virginia'),
+        ('WA', 'Washington'),
+        ('WV', 'West Virginia'),
+        ('WI', 'Wisconsin'),
+        ('WY', 'Wyoming')
+    )
+
+    state = models.CharField(choices=STATE_CHOICES, default='AK')
+
+    # Payment Fields
+
+    card_num = models.IntegerField(max_length=10)
+    card_name = models.CharField(max_length=30)
+
+    # - Most likely an easier way to implement this using regex but this should work for now.
+
+    MONTH_CHOICES = (
+        ('12', 'December'),
+        ('11', 'November'),
+        ('10', 'October'),
+        ('9', 'September'),
+        ('8', 'August'),
+        ('7', 'July'),
+        ('6', 'June'),
+        ('5', 'May'),
+        ('4', 'April'),
+        ('3', 'March'),
+        ('2', 'February'),
+        ('1', 'January')
+    )
+
+    exp_month = models.CharField(choices=MONTH_CHOICES, default=12)
+    exp_year = models.IntegerField(default=2017, max_length=4)
 
 
 @receiver(post_save, sender=User)
@@ -41,4 +129,3 @@ def create_user_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
-
